7f3924ee067e1f477bbe0b70db7793add7b8e3cc
Hennadii_Mariiev <gennadiy.mariev@devit-team.com>
2021-12-13 14:44:32 +0200

    fix kapusta

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -29,7 +29,7 @@ class MailService {
       text: "",
       html:
         `<h1>Hello, dear guest!</h1>` +
-        `<h2>Welcome to our <b>$Kapusta Application</b>` +
+        `<h2>Welcome to our <b>Kapu$ta Application</b>` +
         `</h2><p>Please complete your registration by clicking ` +
         `<a href='${BACKEND_APP_URL}/api/users/verify/${verificationToken}'` +
         `>THIS LINK</a></p><p>We appreciate for your ` +
@@ -41,14 +41,14 @@ class MailService {
     return await this.transporter.sendMail({
       from: SMTP_USER,
       to: friendEmail,
-      subject: "Your friend invites to join $Kapusta App!",
+      subject: "Your friend invites to join Kapu$ta App!",
       text: "",
       html:
         `<h1>Hello, dear ${friendName}!</h1>` +
         `<h2>${userName} invites you to enjoy our App!` +
         `</h2><p>Please feel free while using ` +
         `<a href='${APP_LINK}'` +
-        `>$Kapusta Application</a></p><p>We appreciate for your ` +
+        `>Kapu$ta Application</a></p><p>We appreciate for your ` +
         `connection!</p><h3>Have a nice day!</h3>`,
     });
   }
