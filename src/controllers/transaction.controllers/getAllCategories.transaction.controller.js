f704da7770e02cb325a4be1f613fc1bb542b24eb
Hennadii_Mariiev <marieff1990@gmail.com>
2021-12-15 08:14:13 +0200

    fix main

---
+++ src/controllers/transaction.controllers/getAllCategories.transaction.controller.js
@@ -0,0 +1,13 @@
+const { TransactionService } = require('../../services');
+const { isErrorOrFalsyValue } = require('../../helpers');
+
+const getAllCategories = async (req, res, next) => {
+  const transactions = await TransactionService.getAllCategories(req.body, req.query);
+
+  if (isErrorOrFalsyValue(transactions)) {
+    return next(transactions);
+  }
+  res.status(200).json({ message: 'success', data: { transactions } });
+};
+
+module.exports = { getAllCategories };
--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -5,39 +5,23 @@ const { authenticateUser, transactionAddValidation } = require('../middlewares')
 
 const transactionRouter = express.Router();
 
-transactionRouter.post(
-  '/',
-  asyncWrapper(
-    authenticateUser,
-    transactionValidation,
-    transactionController.add
-  )
-);
+transactionRouter.post('/', asyncWrapper(authenticateUser, transactionAddValidation, transactionController.add));
 
-transactionRouter.get(
-  '/',
-  asyncWrapper(authenticateUser, transactionController.getAll)
-);
+transactionRouter.get('/', asyncWrapper(authenticateUser, transactionController.getAll));
 
-transactionRouter.delete(
-  '/:transactionId',
-  asyncWrapper(authenticateUser, transactionController.remove)
-);
+transactionRouter.delete('/:transactionId', asyncWrapper(authenticateUser, transactionController.remove));
 
 transactionRouter.patch(
   '/:transactionId',
   asyncWrapper(authenticateUser, transactionAddValidation, transactionController.update)
-  
-transactionRouter.get(
-  '/categories',
-  authenticateUser,
-  transactionController.getAllcategories
 );
+
+transactionRouter.get('/categories', asyncWrapper(authenticateUser, transactionController.getAllCategories));
+
 // transactionRouter.get(
 //     "/get",
 //     asyncWrapper(authenticateUser, userValidation, transactionController.add)
 //   );
 // router.get("/get", asyncWrapper(transactionController.getAll));
-
 
 module.exports = { transactionRouter };
