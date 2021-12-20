9c8dad89bee5a2ee771b4e48b47c3b06b480b97c
Hennadii_Mariiev <marieff1990@gmail.com>
2021-12-20 23:01:31 +0200

    add balance to response in get Transactions

--- src/services/transaction.service/getAll.transaction.service.js
+++ src/services/transaction.service/getAll.transaction.service.js
@@ -1,23 +1,19 @@
-const { Transaction } = require("../../model");
-const { formattedDate } = require("../../helpers");
+const { Transaction } = require('../../model');
+const { formattedDate } = require('../../helpers');
 
-const getAll = async ({ user: { _id } }, query) => {
+const getAll = async ({ user: { _id, balance } }, query) => {
   try {
     const searchOptions = { owner: _id };
 
     Object.keys(query).forEach((key) => {
-      if (key !== "user" && query[key] !== undefined)
-        searchOptions[key] = query[key];
+      if (key !== 'user' && query[key] !== undefined) searchOptions[key] = query[key];
     });
 
-    const transactions = await Transaction.find(searchOptions).populate(
-      "owner",
-      "email"
-    );
+    const transactions = await Transaction.find(searchOptions).populate('owner', 'email');
 
     let total = 0;
 
-    if ("startDate" in query) {
+    if ('startDate' in query) {
       const { startDate } = query;
       const endDate = query?.endDate || Date.now();
 
@@ -27,9 +23,7 @@ const getAll = async ({ user: { _id } }, query) => {
             formattedDate(transaction.date) < formattedDate(endDate) &&
             formattedDate(transaction.date) > formattedDate(startDate)
           ) {
-            transaction.type === "income"
-              ? (total += transaction.amount)
-              : (total -= transaction.amount);
+            transaction.type === 'income' ? (total += transaction.amount) : (total -= transaction.amount);
             return [...acc, transaction];
           }
 
@@ -40,13 +34,11 @@ const getAll = async ({ user: { _id } }, query) => {
 
     return transactions
       .map((transaction) => {
-        transaction.type === "income"
-          ? (total += transaction.amount)
-          : (total -= transaction.amount);
+        transaction.type === 'income' ? (total += transaction.amount) : (total -= transaction.amount);
 
         return transaction;
       })
-      .concat({ total });
+      .concat({ total, balance });
   } catch (error) {
     return error;
   }
