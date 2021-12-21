5f7bc4bfe1d929bdced5d845dfb7895a037e7b30
Hennadii_Mariiev <marieff1990@gmail.com>
2021-12-21 22:27:28 +0200

    add notFound page

--- src/controllers/user.controllers/verify.controller.js
+++ src/controllers/user.controllers/verify.controller.js
@@ -1,22 +1,20 @@
-const path = require("path");
-const { UserService } = require("../../services");
-const { isErrorOrFalsyValue, responseWithError } = require("../../helpers");
+const path = require('path');
+const { UserService } = require('../../services');
+const { isErrorOrFalsyValue } = require('../../helpers');
 
-const successVerificationFile = path.join(
-  __dirname,
-  "../../static/success.html"
-);
+const successVerificationFile = path.join(__dirname, '../../static/success.html');
+const notFoundFile = path.join(__dirname, '../../static/notFound.html');
 
-const verify = async (req, res, next) => {
+const verify = async (req, res, _) => {
   const { verificationToken } = req.params;
 
   const user = await UserService.verify(verificationToken);
 
   if (isErrorOrFalsyValue(user)) {
-    return responseWithError(user, next);
+    return res.status(404).sendFile(notFoundFile);
   }
 
-  res.sendFile(successVerificationFile);
+  res.status(200).sendFile(successVerificationFile);
 };
 
 module.exports = { verify };
