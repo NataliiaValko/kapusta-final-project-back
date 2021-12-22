a7469882d4123b6c4f7e80092401d405d4a5617b
Hennadii_Mariiev <gennadiy.mariev@devit-team.com>
2021-12-22 09:17:44 +0200

    change logic with refresh token, small fix Not Found

--- src/controllers/user.controllers/login.controller.js
+++ src/controllers/user.controllers/login.controller.js
@@ -1,7 +1,7 @@
-const { UserService } = require('../../services');
-const { isErrorOrFalsyValue, responseWithError } = require('../../helpers');
-const { cookieOptions } = require('../../config');
-const { UserDTO } = require('../../DTO');
+const { UserService } = require("../../services");
+const { isErrorOrFalsyValue, responseWithError } = require("../../helpers");
+// const { cookieOptions } = require('../../config');
+const { UserDTO } = require("../../DTO");
 
 const login = async (req, res, next) => {
   const { user, tokens } = await UserService.login(req.body);
@@ -11,14 +11,13 @@ const login = async (req, res, next) => {
   }
 
   const userData = UserDTO.getUserSimpleInfo({ user });
-  const { refreshToken, accessToken } = tokens;
 
-  res.cookie('refreshToken', refreshToken, cookieOptions);
+  // res.cookie('refreshToken', refreshToken, cookieOptions);
   res.status(200).json({
-    message: 'success',
+    message: "success",
     data: {
       ...userData,
-      accessToken,
+      ...tokens,
     },
   });
 };
--- src/services/user.services/refresh.service.js
+++ src/services/user.services/refresh.service.js
@@ -1,23 +1,21 @@
-const { Unauthorized } = require('http-errors');
-const { User } = require('../../model');
-const TokenService = require('../token.service');
+const { Unauthorized } = require("http-errors");
+const { User } = require("../../model");
+const TokenService = require("../token.service");
 
 const refresh = async (refreshToken) => {
   try {
     if (!refreshToken) {
-      return new Unauthorized('No refresh token');
+      return new Unauthorized("No refresh token");
     }
 
     const userData = TokenService.validateRefreshToken(refreshToken);
     const userByToken = await TokenService.findUserByToken(refreshToken);
 
     if (!userData || !userByToken) {
-      return new Unauthorized('Unauthorized');
+      return new Unauthorized("Unauthorized");
     }
 
-    const { payload: userEmail } = userData;
-
-    const user = await User.findOne({ userEmail });
+    const user = await User.findById(userByToken._id);
     const tokens = TokenService.generateTokens(user.email);
     await TokenService.saveRefreshToken(user._id, tokens.refreshToken);
 
